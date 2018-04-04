74ac7e2855bc282cecfb23e470100a8e1167509a
Alexandre <36648124+alexandreb09@users.noreply.github.com>
2018-04-05 00:55:43 +0200

    Add files via upload
    Lit un fichier pdf et extrait les mots clés... 
    La recherche de l'ensemble des mots-clés est à améliorer...

--- LecturePDF.py
+++ LecturePDF.py
@@ -1,13 +1,57 @@
-import PyPDF2
+import textract
 
-fichier = "monfichier.pdf"
 
-def get_pdf_content_lines(fich):
-    with open(fich, "rb") as f:                                     # with open ... as : ouverture fichier dans le bloc et fermeture en sortie
-        pdf_reader = PyPDF2.PdfFileReader(f)
-        for page in pdf_reader.pages:
-            for line in page.extractText().splitlines():
-                yield line
+nomFichier = "Rapport1.pdf"
+print("Fichier étudié : ",nomFichier)
+ListeMotCle = []
 
-for ligne in get_pdf_content_lines(fichier):
-    print(ligne)
+
+def LongueurChaine(chaine):                             # Donne l'indice de fin des mots clés
+    if chaine[0] == ' ': chaine=chaine[1:]
+    nbMot = nbLigne = 0                                 # et la chaine sans les répétitions
+    ind = 0
+    while nbMot < 10 and nbLigne < 2:
+        if chaine[ind] == ',': nbMot+=1
+        if chaine[ind] == '\n': nbLigne+=1
+        ind+=1
+    return (ind,chaine[:ind+1])
+
+def AffichageMotCle(Liste):
+    print("\nListe des mots clés :")
+    for elt in Liste:
+        print(elt)
+
+"""
+def AjoutMotsCles(chaine,ListeMotCle):          #fonction split (ici sans espace)
+    print('ma chaine : ',chaine)
+    cour = debMot = 0
+    while debMot + cour  < fin:
+        if chaine[debMot + cour] == ',':
+            print(chaine[debMot:debMot+cour])
+            ListeMotCle.append(chaine[debMot:debMot+cour])
+            debMot = debMot + cour + 2
+            cour = 0
+        else : cour+=1
+    return(ListeMotCle)
+
+def RecherDebMotCle(text):
+    deb = 0
+    while deb < longueur - l2 and text[deb:deb+l2]!=stop:       #recherche de l'occurence
+        deb +=1
+
+"""
+
+
+stop = "Mots-clés"
+l2 = len(stop)
+
+text = textract.process(nomFichier, encoding='utf-8',method='pdfminer')
+text = bytes.decode(text)
+
+
+deb = text.index(stop)
+fin,chaine = LongueurChaine(text[deb+l2+2:])
+chaine = chaine.replace('\n',' ')                                   # suppresion des \n
+chaine = chaine.replace(', ',',')
+ListeMotCle = chaine.split(',')
+AffichageMotCle(ListeMotCle)
